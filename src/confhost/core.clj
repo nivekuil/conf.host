e0d797ff59810930213bf9b8973f43fa50834306
Kevin Liu <mail@nivekuil.com>
2016-06-23 20:49:07 -0700

    Use immutant instead of jetty

--- src/confhost/core.clj
+++ src/confhost/core.clj
@@ -1,21 +1,26 @@
 (ns confhost.core
-  (:require [compojure.handler :as handler]
-            [compojure.response :as response]
-            [compojure.route :as route]
-            [ring.adapter.jetty :refer [run-jetty]]
+  (:require [compojure.core :refer [routes]]
+            [compojure.route :refer [resources]]
+            [immutant.web :as web]
             [ring.middleware.cookies :refer [wrap-cookies]]
             [ring.middleware.params :refer [wrap-params]])
   (:require [confhost.routes]))
 
 (def app
-  (-> (compojure.core/routes
-       confhost.routes/index
-       (route/resources "/")
-       (confhost.routes/not-found))
+  (-> (routes
+       #'confhost.routes/routes
+       (resources "/"))
       wrap-cookies wrap-params))
+#_
+(do (web/stop server)
+    (def server (web/run app {:host "192.168.1.18" :port 3000})))
+
+(defn -main [& args]
+  (defonce server (web/run app {:host "192.168.1.18" :port 3000})))
+
+#_
+(defn -main [& args]
+  (defonce server (run-jetty app {:port 3000 :join? false})))
 #_
 (do (.stop server)
     (def server (run-jetty app {:port 3000 :join? false})))
-
-(defn -main []
-  (def server (run-jetty app {:port 3000 :join? false})))
