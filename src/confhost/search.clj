e778156d396ce83b5bf3f3b65b824cbdba3e4705
Kevin Liu <mail@nivekuil.com>
2016-07-04 09:43:36 -0700

    Add search

---
+++ src/confhost/search.clj
@@ -0,0 +1,25 @@
+(ns confhost.search
+  (:require [clojurewerkz.elastisch.native :as es]
+            [clojurewerkz.elastisch.query :as q]
+            [clojurewerkz.elastisch.native.document :as esd]))
+
+(def endpoints [["127.0.0.1" 9300]])
+(def cluster-name "elasticsearch")
+(def conn (es/connect endpoints {"cluster.name" cluster-name}))
+(def index "resource")
+
+(defn query-user
+  "Get all documents belonging to some user and return the metadata needed to
+  generate the file browser interface."
+  [username]
+  (let [search (:hits
+                (esd/search conn index username
+                            {:query {:type {:value username}} :size 500}))
+        num-results (:total search)
+        results (:hits search)]
+    {:num-results num-results
+     :results (map (fn [result]
+                     (assoc (select-keys
+                             (:_source result)
+                             [:extension :filename :filesize :mtime])
+                            :hash (:_id result))) results)}))
