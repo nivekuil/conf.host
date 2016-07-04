e1e5bc95e86cd83e575f4b9a37d06bf16fc5a426
Kevin Liu <mail@nivekuil.com>
2016-07-04 09:53:07 -0700

    Add render

---
+++ src/confhost/render.clj
@@ -0,0 +1,23 @@
+(ns confhost.render
+  (:require [hiccup.core :refer [html]]
+            [simple-time.core :as time]
+            [clojure.contrib.humanize :as humanize]))
+
+(def file-base "http://ipfs.io/ipfs/")
+
+(defn files [results]
+  (html
+   (for [x results]
+     (let [extension (:extension x)
+           filename (:filename x)
+           filesize (:filesize x)
+           mtime (:mtime x)]
+       [:tr
+        [:td {:class "extension"} extension]
+        [:td {:class "filename"}
+         [:a {:href (str file-base (:hash x))} filename]]
+        [:td {:class "filesize" :sorttable_customkey filesize}
+         (if (> filesize 1000)
+           (humanize/filesize filesize) (str filesize "B"))]
+        [:td {:class "mtime" :title mtime}
+         (time/format (time/datetime mtime) "YYYY-MM-dd")]]))))
