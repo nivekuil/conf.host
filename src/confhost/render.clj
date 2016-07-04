377f8fddf647df4f4d0c1bd1318bf729084ad0c5
Kevin Liu <mail@nivekuil.com>
2016-07-04 10:33:05 -0700

    Use defhtml macro.  Fewer parens!

--- src/confhost/render.clj
+++ src/confhost/render.clj
@@ -1,22 +1,21 @@
 (ns confhost.render
-  (:require [hiccup.core :refer [html]]
+  (:require [hiccup.def :refer [defhtml]]
             [clojure.contrib.humanize :as humanize]))
 
 (def file-base "http://ipfs.io/ipfs/")
 
-(defn files [results]
-  (html
-   (for [x results]
-     (let [extension (:extension x)
-           filename (:filename x)
-           filesize (:filesize x)
-           mtime (:mtime x)]
-       [:tr
-        [:td {:class "extension"} extension]
-        [:td {:class "filename"}
-         [:a {:href (str file-base (:hash x))} filename]]
-        [:td {:class "filesize" :sorttable_customkey filesize}
-         (if (> filesize 1000)
-           (humanize/filesize filesize) (str filesize "B"))]
-        [:td {:class "mtime" :sorttable_customkey mtime :title mtime}
-         (humanize/datetime mtime)]]))))
+(defhtml files [results]
+  (for [x results]
+    (let [extension (:extension x)
+          filename (:filename x)
+          filesize (:filesize x)
+          mtime (:mtime x)]
+      [:tr
+       [:td {:class "extension"} extension]
+       [:td {:class "filename"}
+        [:a {:href (str file-base (:hash x))} filename]]
+       [:td {:class "filesize" :sorttable_customkey filesize}
+        (if (> filesize 1000)
+          (humanize/filesize filesize) (str filesize "B"))]
+       [:td {:class "mtime" :sorttable_customkey mtime :title mtime}
+        (humanize/datetime mtime)]])))
