708ec1d0c24995f0e271a3e2ab1959dab2f84528
Kevin Liu <mail@nivekuil.com>
2016-07-09 07:29:27 -0700

    Display icons

--- src/confhost/render.clj
+++ src/confhost/render.clj
@@ -1,21 +1,28 @@
 (ns confhost.render
-  (:require [hiccup.def :refer [defhtml]]
-            [clojure.contrib.humanize :as humanize]))
+  (:require [confhost.util :as util]
+            [hiccup.def :refer [defhtml]]
+            [clojure.contrib.humanize :as humanize]
+            [clojure.string :as string]))
 
-(def file-base "http://ipfs.io/ipfs/")
+(def base-uri "http://ipfs.io/ipfs/")
+(def icon-dir "/icons/")
+;;; Set of file extensions that have associated icons
+(def icons (confhost.util/get-filenames (str "resources/public" icon-dir)))
 
 (defhtml files [results]
   (for [x results]
-    (let [extension (:extension x)
+    (let [ext (:extension x)
           filename (:filename x)
           filesize (:filesize x)
           mtime (:mtime x)]
       [:tr
-       [:td {:class "extension"} extension]
+       [:td {:class "extension" :sorttable_customkey ext :title ext}
+        (if (contains? icons ext) [:img {:src (str icon-dir ext ".svg")}] ext)]
        [:td {:class "filename"}
-        [:a {:href (str file-base (:hash x))} filename]]
+        [:a {:href (str base-uri (:hash x))} filename]]
        [:td {:class "filesize" :sorttable_customkey filesize}
         (if (>= filesize 1000)
+          ;; humanize prints single bytes as decimal, which is ugly
           (humanize/filesize filesize) (str filesize "B"))]
        [:td {:class "mtime" :sorttable_customkey mtime :title mtime}
         (humanize/datetime mtime)]])))
