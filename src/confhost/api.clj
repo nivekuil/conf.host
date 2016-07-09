46e21367e4629b80cda007deaf906e74697bd24d
Kevin Liu <mail@nivekuil.com>
2016-07-09 07:43:59 -0700

    Add api routes

---
+++ src/confhost/api.clj
@@ -0,0 +1,40 @@
+(ns confhost.api
+  (:require [confhost.search :as search]
+            [clojure.string :as string]
+            [compojure.core :refer [defroutes GET]]
+            [compojure.route :refer [not-found]]
+            [clj-ipfs-api.core :as ipfs]))
+
+(defn wrap-api
+  "((TODO)) Match the Content-Type of a response to the provided Accept header,
+  or text/plain if there is none."
+  [response]
+  {})
+
+(def api-index
+  (GET "/" [request]
+       "Welcome to the conf.host API!<br>Valid paths:<br>
+/ipfs/:hash -- get the contents of an IPFS hash<br>
+/:username/hashes -- get list of IPFS hashes associated with the given user"))
+
+(def api-user-hashes
+  (GET "/:username/hashes" [username]
+       (let [query (search/query-user username)]
+         (string/join " " (map :hash
+                               (map search/parse-files
+                                    (:hits query)))))))
+
+(def api-user-file
+  ;; Need the regex for :filename because compojure treats "." as a separator
+  (GET ["/:username/raw/:filename", :filename #"[[a-z]\.]+"]
+       [username filename]
+       (let [query (search/query-file username filename)]
+         (ipfs/cat (-> query :hits first :_id)))))
+
+(def api-ipfs-hash
+  "Get a file by its IPFS hash"
+  (GET ["/ipfs/:hash"] [hash]
+       (ipfs/cat hash)))
+
+(defroutes api-routes #'api-index #'api-ipfs-hash #'api-user-file
+  #'api-user-hashes (not-found "API 404"))
