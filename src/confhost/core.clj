75a920cb75d317bdc053e847edc039182d52d11e
Kevin Liu <mail@nivekuil.com>
2016-07-11 20:23:26 -0700

    Add code to start api server

--- src/confhost/core.clj
+++ src/confhost/core.clj
@@ -1,22 +1,32 @@
 (ns confhost.core
-  (:require [compojure.core :refer [routes]]
+  (:require [confhost.routes :refer [routes]]
+            [confhost.api :refer [api-routes]]
+            [compojure.core :as compojure]
             [compojure.route :refer [resources]]
             [immutant.web :as web]
             [ring.middleware.cookies :refer [wrap-cookies]]
-            [ring.middleware.params :refer [wrap-params]])
-  (:require [confhost.routes]))
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.middleware.resource :refer [wrap-resource]]))
 
 (def app
-  (-> (routes
-       #'confhost.routes/routes
-       (resources "/"))
-      wrap-cookies wrap-params))
+  (-> #'routes
+      (wrap-cookies) (wrap-params) (wrap-resource "public")))
+
+(def api
+  (-> #'api-routes
+      (wrap-cookies) (wrap-params) (wrap-resource "public")))
 #_
-(do (web/stop server)
-    (def server (web/run app {:host "192.168.1.18" :port 3000})))
+(do (web/run app {:host "0.0.0.0" :port 3000})
+    (web/run api {:host "0.0.0.0" :port 3000
+                  :path "/api"})
+    (web/stop {:host "0.0.0.0 :port 3000"})
+    (web/stop {:host "0.0.0.0 :port 3000" :path "/api"})
+    (def server (web/run app {:host "0.0.0.0" :port 3000}))
+    (def api-server (web/run api {:host "0.0.0.0" :port 3000
+                                  :path "/api"})))
 
 (defn -main [& args]
-  (defonce server (web/run app {:host "192.168.1.18" :port 3000})))
+  (defonce server (web/run app {:host "localhost" :port 3000})))
 
 #_
 (defn -main [& args]
